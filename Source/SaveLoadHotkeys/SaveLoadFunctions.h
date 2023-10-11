9252c20e827dce556387ce45ede7fa5dd51979b1
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-10-11 09:58:46 +0300

    1.0.0 release

---
+++ Source/SaveLoadHotkeys/SaveLoadFunctions.h
@@ -0,0 +1,53 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FGGameMode.h"
+#include "FGSaveSession.h"
+#include "FGSaveSystem.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SaveLoadFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVELOADHOTKEYS_API USaveLoadFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	static void Save(UObject* WorldContext)
+	{
+		if(!WorldContext) return;
+		
+		auto Session = UFGSaveSession::Get(WorldContext);
+		Session->Autosave();
+	}
+
+	UFUNCTION(BlueprintCallable)
+	static void Load(UObject* WorldContext)
+	{
+		if(!WorldContext) return;
+		
+		auto SaveSystem = UFGSaveSystem::Get(WorldContext);
+		auto Sessions = SaveSystem->NativeEnumerateSaveGames();
+		auto SessionName = Cast<AFGGameMode>(WorldContext->GetWorld()->GetAuthGameMode())->GetSaveSessionName();
+
+		FSaveHeader LatestSaveHeader = FSaveHeader();
+		for(auto Session : Sessions)
+		{
+			if(Session.SessionName != SessionName) continue;
+			
+			if(Session.PlayDurationSeconds > LatestSaveHeader.PlayDurationSeconds) LatestSaveHeader = Session;
+		}
+
+
+		auto Options = TMap<FString, FString>();
+		auto Player = UGameplayStatics::GetPlayerController(WorldContext, 0);
+		SaveSystem->LoadSaveFile(LatestSaveHeader, Options, Player);
+	}
+	
+};
---
+++ Source/SaveLoadHotkeys/SaveLoadHotkeysModule.cpp
@@ -0,0 +1,7 @@
+#include "SaveLoadHotkeysModule.h"
+
+void FSaveLoadHotkeysModule::StartupModule() {
+
+}
+
+IMPLEMENT_GAME_MODULE(FSaveLoadHotkeysModule, SaveLoadHotkeys);
