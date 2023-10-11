fbb7bf6dd8b1ae1a024874b4a1b58860f92c1c83
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-10-11 17:49:54 +0300

    Loading warning, mod icon

--- Source/SaveLoadHotkeys/SaveLoadFunctions.h
+++ Source/SaveLoadHotkeys/SaveLoadFunctions.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "FGGameMode.h"
+#include "FGGameState.h"
 #include "FGSaveSession.h"
 #include "FGSaveSystem.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
@@ -48,6 +49,25 @@ public:
 		auto Options = TMap<FString, FString>();
 		auto Player = UGameplayStatics::GetPlayerController(WorldContext, 0);
 		SaveSystem->LoadSaveFile(LatestSaveHeader, Options, Player);
+	}
+
+	UFUNCTION(BlueprintPure)
+	static double GetSecondsSinceLastSave(UObject* WorldContext)
+	{
+		auto SaveSystem = UFGSaveSystem::Get(WorldContext);
+		auto Sessions = SaveSystem->NativeEnumerateSaveGames();
+		auto GameState = Cast<AFGGameState>(UGameplayStatics::GetGameState(WorldContext));
+		auto const SessionName = GameState->GetSessionName();
+
+		FSaveHeader LatestSaveHeader = FSaveHeader();
+		for(auto Session : Sessions)
+		{
+			if(Session.SessionName != SessionName) continue;
+			
+			if(Session.PlayDurationSeconds > LatestSaveHeader.PlayDurationSeconds) LatestSaveHeader = Session;
+		}
+
+		return GameState->GetTotalPlayDuration() - LatestSaveHeader.PlayDurationSeconds;
 	}
 	
 };
